9dcdc84945742dc27d17cd86485fa66f9d0b0880
Jadiê Oliveira <jadieoliveira00@gmail.com>
2023-03-09 11:13:20 -0300

    new screen

--- LoginViewCode/Register/Screen/RegisterScreen.swift
+++ LoginViewCode/Register/Screen/RegisterScreen.swift
@@ -16,11 +16,49 @@ class RegisterScreen: UIView {
         img.contentMode = .scaleAspectFit
         return img
     }()
+    
+    lazy var emailTextField:UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.autocorrectionType = .no
+        tf.backgroundColor = .white
+        tf.borderStyle = .roundedRect
+        tf.keyboardType = .emailAddress
+        tf.placeholder = "Digite seu Email:"
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.textColor = .darkGray
+        return tf
+    }()
+    
+    lazy var passwordTextField:UITextField = {
+        let tf = UITextField()
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        tf.autocorrectionType = .no
+        tf.backgroundColor = .white
+        tf.borderStyle = .roundedRect
+        tf.keyboardType = .default
+        tf.placeholder = "Digite sua senha:"
+        tf.isSecureTextEntry = true
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.textColor = .darkGray
+        return tf
+    }()
+    
+    lazy var registerButton:UIButton = {
+       let btn = UIButton()
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setTitle("Cadastrar", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        btn.clipsToBounds = true
+        btn.layer.cornerRadius = 7.5
+        btn.backgroundColor = UIColor(red: 3/255, green: 58/255, blue: 51/255, alpha: 1.0)
+        return btn
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.addSubview(self.userImageView)
         self.configScreenBackground()
+        self.configSuperView()
         self.setUpConstraints()
     }
     
@@ -28,8 +66,16 @@ class RegisterScreen: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
     private func configScreenBackground() {
         self.backgroundColor = UIColor(red: 24/255, green: 117/255, blue: 104/255, alpha: 1.0)
+    }
+    
+    private func configSuperView() {
+        self.addSubview(self.userImageView)
+        self.addSubview(self.emailTextField)
+        self.addSubview(self.passwordTextField)
+        self.addSubview(self.registerButton)
     }
     
     private func setUpConstraints() {
@@ -37,7 +83,22 @@ class RegisterScreen: UIView {
             self.userImageView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 20),
             self.userImageView.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             self.userImageView.widthAnchor.constraint(equalToConstant: 150),
-            self.userImageView.heightAnchor.constraint(equalToConstant: 150)
+            self.userImageView.heightAnchor.constraint(equalToConstant: 150),
+            
+            self.emailTextField.topAnchor.constraint(equalTo: self.userImageView.bottomAnchor, constant: 40),
+            self.emailTextField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+            self.emailTextField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            self.emailTextField.heightAnchor.constraint(equalToConstant: 45),
+            
+            self.passwordTextField.topAnchor.constraint(equalTo: self.emailTextField.bottomAnchor, constant: 15),
+            self.passwordTextField.leadingAnchor.constraint(equalTo: self.emailTextField.leadingAnchor),
+            self.passwordTextField.trailingAnchor.constraint(equalTo: self.emailTextField.trailingAnchor),
+            self.passwordTextField.heightAnchor.constraint(equalTo: self.emailTextField.heightAnchor),
+            
+            self.registerButton.topAnchor.constraint(equalTo: self.passwordTextField.bottomAnchor, constant: 20),
+            self.registerButton.leadingAnchor.constraint(equalTo: self.emailTextField.leadingAnchor),
+            self.registerButton.trailingAnchor.constraint(equalTo: self.emailTextField.trailingAnchor),
+            self.registerButton.heightAnchor.constraint(equalTo: self.emailTextField.heightAnchor),
             
         ])
     }
